811815e35c532c68787450ec2d381957b7945e1e
Brenton Salmi <brent@faradayrf.com>
2017-07-23 11:32:39 -0700

    Adding restart ability.

--- Applications/Data/client_programs/sockserver.py
+++ Applications/Data/client_programs/sockserver.py
@@ -7,27 +7,38 @@ server_address = ('localhost', 10000)
 def listen():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(server_address)
-
-    print "Bind Complete."
-    sock.listen(1)
-    print "Socket now listening."
+    count = 0
 
     while True:
-        current_connection, address = sock.accept()
-        while True:
-            #time.sleep(0.5)
-            data = current_connection.recv(2048)
+        print "Bind Complete."
+        sock.listen(1)
+        count += 1
+        print "Socket now listening.", count
+        connected = True
 
-            if len(data)>0:
-                if data == 'quit\r\n':
-                    current_connection.shutdown(1)
-                    current_connection.close()
-                    break
-                else:
-                    print repr(data), len(data), type(data)
-                    print data
-            else:
-                pass
+        while connected == True:
+            try:
+                current_connection, address = sock.accept()
+
+                while connected == True:
+                    #time.sleep(0.5)
+                    data = current_connection.recv(2048)
+
+                    if len(data)>0:
+                        if data == 'quit\r\n':
+                            current_connection.shutdown(1)
+                            current_connection.close()
+                            break
+                        else:
+                            print repr(data), len(data), type(data)
+                            print data
+                    else:
+                        pass
+            except socket.error as e:
+                print "Connection terminated forcibly."
+                connected = False
+                #current_connection.shutdown(1)
+                current_connection.close()
 
 
 if __name__ == "__main__":
