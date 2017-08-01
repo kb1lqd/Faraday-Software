ad4c49c8e1284a651d797fde82aced1c480516c3
Brenton Salmi <brent@faradayrf.com>
2017-08-01 00:13:34 -0700

    Making rx client print simple data only.

--- Applications/Data/client_programs/client2.py
+++ Applications/Data/client_programs/client2.py
@@ -3,32 +3,32 @@ import random
 import struct
 import time
 import sys
-import base64
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 10011              # Reserve a port for your service.
 
-RXDATASIZE = 42
 
-s.connect((host, port))
-print s
+
 
 
 while True:
+    s = socket.socket()         # Create a socket object
+    host = socket.gethostname() # Get local machine name
+    port = 10011               # Reserve a port for your service.
+    s.connect((host, port))
     try:
         while True:
-            time.sleep(0.001)
+            time.sleep(0.01)
             s.sendall(" ")
             temp = s.recv(4096)
-            print "RX: ", temp, len(temp)#base64.b64decode(temp[:56])
-            print "RX CLEAN: ", temp[:RXDATASIZE], '\n'
+            if temp != 'No Data! Goodbye.':
+                print "RX: ", repr(temp)
             #if not temp: break
 
 
         #time.sleep(0.1)
 
     except socket.error as e:
-        print e
-        sys.exit(1)
+        #print e
+        s.close()
+        #sys.exit(1)
+    time.sleep(0.01)
 s.close                     # Close the socket when done
