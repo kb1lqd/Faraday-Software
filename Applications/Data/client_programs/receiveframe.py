8cf22749e86319a8b52821c1300d27dbde53109c
Brenton Salmi <brent@faradayrf.com>
2017-08-02 09:40:02 -0700

    Adding recieve frame that thorugh command line returns standard output of the recieve buffer

---
+++ Applications/Data/client_programs/receiveframe.py
@@ -0,0 +1,40 @@
+import socket               # Import socket module
+import time
+import argparse
+
+# Get arguments
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
+# Parse arguments
+
+
+# Create data fragments as needed
+
+
+alive = True
+
+
+
+s = socket.socket()         # Create a socket object
+#s.setblocking(False)
+s.settimeout(2)
+host = socket.gethostname() # Get local machine name
+port = 10011               # Reserve a port for your service.
+s.connect((host, port))
+try:
+    while alive:
+        time.sleep(0.01)
+        s.sendall(" ")
+        temp = s.recv(4096)
+        if temp != 'No Data! Goodbye.':
+            print temp
+        else:
+            alive = False
+except StandardError as e:
+    print e
+    pass
+
+finally:
+    print"Closing socket connection."
+    s.close()                     # Close the socket when done
