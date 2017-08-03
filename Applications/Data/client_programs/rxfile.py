7e53d4a13d3215f197c38c4a07ca815f14e35683
Brenton Salmi <brent@faradayrf.com>
2017-08-02 23:32:57 -0700

    Working file transfer with text files! May need encoding for binary data through stdin and out...

--- Applications/Data/client_programs/rxfile.py
+++ Applications/Data/client_programs/rxfile.py
@@ -7,14 +7,30 @@ import sys
 import json
 
 # Get arguments
-#parser = argparse.ArgumentParser()
-#parser.add_argument("--json", help="Interpret stdin data as JSON encoded data", action="store_true")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="Name of file to save completed assembly as")
+parser.add_argument("--json", help="Interpret stdin data as JSON encoded data", action="store_true")
+args = parser.parse_args()
 
 # Get stdin data
 
 rxdata_list = []
 rxdata = sys.stdin
-for line in rxdata:
-    #rxdata_list.append(line)
-    print json.loads(line)
+
+if args.json:
+    for line in rxdata:
+        temp = json.loads(line)
+        for item in temp:
+            rxdata_list.append(item)
+else:
+    for line in rxdata:
+        rxdata_list.append(line)
+
+
+data = ''.join(rxdata_list)
+
+file = open(args.filename, 'wb')
+for item in data:
+    file.write(item)
+
+file.close()
