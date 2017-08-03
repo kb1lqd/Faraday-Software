b2ee3ac774196473b31e458bf81264cee5409767
Brenton Salmi <brent@faradayrf.com>
2017-08-02 22:42:45 -0700

    Adding JSON output support to frame parser so that packet boundries are maintained through stdin and stdout

--- Applications/Data/client_programs/parseframe.py
+++ Applications/Data/client_programs/parseframe.py
@@ -1,35 +1,39 @@
 import argparse
 import sys
 import bytestuff
+import json
 
 # Get arguments
-#parser = argparse.ArgumentParser()
-#parser.add_argument("data", help="Data string to be transmitted.")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--json", help="Output JSON formatted parsed frames to stdout instead of raw frame contents", action="store_true")
+args = parser.parse_args()
 
 rxframeparser = bytestuff.rxparse()
 
 # Parse arguments
 #data = args.data
 data = sys.stdin
+
 datalist = []
 datastr = ''
 for line in data:
-    datalist.append(line)
     datastr = datastr + line
 
-#for item in datalist:
-#    sys.stdout.write(item)
-
-#sys.stdout.write("Parsing")
-
 for i in range(0,len(datastr)):
-    #sys.stdout.write(datastr[i])
+    # Parse frames
     test = rxframeparser.parseframe(datastr[i], bytestuff.framing_startbyte, bytestuff.framing_stopbyte, bytestuff.framing_escapebyte)
-    try:
-        sys.stdout.write(test)
-    except TypeError as e:
-        #print "ERROR:", e
+    if test != None:
+        datalist.append(test)
+    else:
+        # Ignore
         pass
 
-#sys.stdout.write("Parsed")
+if args.json:
+    sys.stdout.write(json.dumps(datalist))
+else:
+    for item in datalist:
+        try:
+            sys.stdout.write(item)
+        except TypeError as e:
+            # print "ERROR:", e
+            pass
