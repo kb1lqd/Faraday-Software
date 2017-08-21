ec50b19f896f285b829363b95de6054ef876e80c
Brent <brent@faradayrf.com>
2017-08-20 22:29:56 -0700

    Removing extra send and renaming.

--- Applications/Data/TUN_ADAPTER/send.py
+++ Applications/Data/TUN_ADAPTER/send.py
@@ -7,33 +7,33 @@ import sys
 import os
 
 
+def send(data):
+	#data = ''
 
-data = ''
+	# Get arguments
+	#parser = argparse.ArgumentParser()
+	#parser.add_argument("fragsize", type=int, help="Fragment size in bytes of data", default=16)
+	#parser.add_argument("--data", help="Data string to be transmitted.")
+	#args = parser.parse_args()
 
-# Get arguments
-parser = argparse.ArgumentParser()
-#parser.add_argument("fragsize", type=int, help="Fragment size in bytes of data", default=16)
-parser.add_argument("--data", help="Data string to be transmitted.")
-args = parser.parse_args()
-
-# Parse arguments
-#STRUCTSIZE = args.fragsize
-if args.data is not None:
-    data = args.data
-else:
-    for line in sys.stdin:
-        data = data + line  # Concatenate stdin lines to single string
+	# Parse arguments
+	#STRUCTSIZE = args.fragsize
+	#if args.data is not None:
+	#    data = args.data
+	#else:
+	#    for line in sys.stdin:
+	#	data = data + line  # Concatenate stdin lines to single string
 
 
 
-# Open socket connection to Faraday data server
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (socket.gethostname(), 10000)
-print 'connecting to {0} port {1}'.format(server_address[0], server_address[1])
-sock.connect((server_address))
+	# Open socket connection to Faraday data server
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_address = (socket.gethostname(), 10000)
+	print 'connecting to {0} port {1}'.format(server_address[0], server_address[1])
+	sock.connect((server_address))
 
-# Send data
-sock.sendall(data)
+	# Send data
+	sock.sendall(data)
 
-# Close socket
-sock.close()
+	# Close socket
+	sock.close()
