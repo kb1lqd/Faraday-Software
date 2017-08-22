06b94e2113c3a18a1ce0d44f5e3dbd33075ad08a
Brent <brent@faradayrf.com>
2017-08-21 23:37:14 -0700

    Updating timeouts in receive.

--- Applications/Data/TUN_ADAPTER/faraday_receive_tun.py
+++ Applications/Data/TUN_ADAPTER/faraday_receive_tun.py
@@ -4,43 +4,40 @@ import argparse
 import sys
 import dpkt
 
-# Get arguments
-#parser = argparse.ArgumentParser()
-#args = parser.parse_args()
-
-# Parse arguments
-
-
-# Create data fragments as needed
-
 
 alive = True
 
 
 
-s = socket.socket()         # Create a socket object
-#s.setblocking(False)
-s.settimeout(5)
-host = socket.gethostname() # Get local machine name
-port = 10011               # Reserve a port for your service.
-s.connect((host, port))
-try:
-    while alive:
-        time.sleep(0.01)
-        s.sendall(" ")
-        temp = s.recv(4096)
-        if temp != 'No Data! Goodbye.':
-            #temp = dpkt.udp.UDP(temp)
-            #temp = dpkt.ip.IP(temp)
-            print repr(temp)
-            #sys.stdout.write(temp)
-        else:
-            alive = False
-except StandardError as e:
-    print e
-    pass
 
 
-finally:
-    print"Closing socket connection."
-    s.close()                     # Close the socket when done
+while True:
+    s = socket.socket()  # Create a socket object
+    # s.setblocking(False)
+    #s.settimeout(5)
+    host = socket.gethostname()  # Get local machine name
+    port = 10011  # Reserve a port for your service.
+    s.connect((host, port))
+    print "Connected"
+    try:
+        while alive:
+            time.sleep(0.01)
+            s.sendall(" ")
+            temp = s.recv(2048)
+            if temp != 'No Data! Goodbye.':
+                #temp = dpkt.udp.UDP(temp)
+                #temp = dpkt.ip.IP(temp)
+                print repr(temp)
+                #sys.stdout.write(temp)
+            else:
+                alive = False
+    except StandardError as e:
+        #print e
+        pass
+    except socket.timeout as e:
+        pass
+
+
+    finally:
+        #print"Closing socket connection."
+        s.close()                     # Close the socket when done
